c98937d78473b54a01e7782412bdd38a27db68e9
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2013-08-05 23:14:58 +0400

    add place to application

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -3,4 +3,7 @@ class Web::WelcomeController < Web::ApplicationController
     @news = News.published_at_year(2013).last 5
     @photo_album = PhotoAlbumDecorator.decorate PhotoAlbum.welcome_album
   end
+
+  def place
+  end
 end
